c30e8fef52a730e4bd3181655a11b5d4bfbf257d
jeffkarr <jkarr001@gmail.com>
2023-10-18 20:23:58 -0500

    Corrected error in ManageResize function
    Reinstated if screensize > 1200 to execute all timelines except home-reveal. The home-reveal timeline will execute regardless of screensize.

--- js/app.js
+++ js/app.js
@@ -60,18 +60,18 @@ function manageResize() {
 	let screenHeight = document.documentElement.clientHeight;
 	ScrollTrigger.refresh();
 
-	// if (screenWidth > 1200) {
-		const homeReveal = gsap.utils.toArray('.home-reveal')
-		homeReveal.forEach((text, i) => {
-			ScrollTrigger.create({
-				trigger: text,
-				toggleClass: 'active',
-				start: "top 90%",
-				end: "top 60%",
-				// markers: true
-			})
-		})
+    const homeReveal = gsap.utils.toArray('.home-reveal')
+    homeReveal.forEach((text, i) => {
+        ScrollTrigger.create({
+            trigger: text,
+            toggleClass: 'active',
+            start: "top 90%",
+            end: "top 60%",
+            // markers: true
+        })
+    })
 
+	if (screenWidth > 1200) {
 		const reveal250s = gsap.utils.toArray('.content-250')
 		reveal250s.forEach((card, i) => {
 			ScrollTrigger.create({
@@ -426,23 +426,23 @@ function manageResize() {
 			delay: 1.5,
 			duration: 1.25
 		});
-	// } else {
-	// 	$(".content-250").addClass("active");
-	// 	$(".content-350").addClass("active");
-	// 	let exp1ElHeight = $("#exp-1").css("height");
-	// 	let exp1Idx = exp1ElHeight.indexOf('px');
-	// 	exp1ElHeight = parseInt(exp1ElHeight.substr(0, exp1Idx) );
-	// 	let exp2ElHeight = $("#exp-2").css("height");
-	// 	let exp2Idx = exp2ElHeight.indexOf('px');
-	// 	exp2ElHeight = parseInt(exp2ElHeight.substr(0, exp2Idx));
-	// 	let exp3ElHeight = $("#exp-3").css("height");
-	// 	let exp3Idx = exp3ElHeight.indexOf('px');
-	// 	exp3ElHeight = parseInt(exp3ElHeight.substr(0, exp3Idx));
-	// 	let exp4ElHeight = $("#exp-4").css("height");
-	// 	let exp4Idx = exp4ElHeight.indexOf('px');
-	// 	exp4ElHeight = parseInt(exp4ElHeight.substr(0, exp4Idx));
-	// 	let expHeightSum = exp1ElHeight + exp2ElHeight + exp3ElHeight + exp4ElHeight;
-	// 	$(".timeline").css("height", `${expHeightSum}`);
-	// 	$("#about-img-wrapper").removeClass("pl-3");
-	// }
+	} else {
+		$(".content-250").addClass("active");
+		$(".content-350").addClass("active");
+		let exp1ElHeight = $("#exp-1").css("height");
+		let exp1Idx = exp1ElHeight.indexOf('px');
+		exp1ElHeight = parseInt(exp1ElHeight.substr(0, exp1Idx) );
+		let exp2ElHeight = $("#exp-2").css("height");
+		let exp2Idx = exp2ElHeight.indexOf('px');
+		exp2ElHeight = parseInt(exp2ElHeight.substr(0, exp2Idx));
+		let exp3ElHeight = $("#exp-3").css("height");
+		let exp3Idx = exp3ElHeight.indexOf('px');
+		exp3ElHeight = parseInt(exp3ElHeight.substr(0, exp3Idx));
+		let exp4ElHeight = $("#exp-4").css("height");
+		let exp4Idx = exp4ElHeight.indexOf('px');
+		exp4ElHeight = parseInt(exp4ElHeight.substr(0, exp4Idx));
+		let expHeightSum = exp1ElHeight + exp2ElHeight + exp3ElHeight + exp4ElHeight;
+		$(".timeline").css("height", `${expHeightSum}`);
+		$("#about-img-wrapper").removeClass("pl-3");
+	}
 }
